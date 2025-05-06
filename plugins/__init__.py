068919f3cb72e560f2b696a76a6a0411f5423dc2
Yiheng Tao <ruby092977@gmail.com>
2025-05-05 21:33:47 -0700

    Remove unused plugin import and update documentation in __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -3,9 +3,3 @@
 This package contains plugins that extend the MCP toolset.
 Each subdirectory contains a separate plugin implementation.
 """
-
-# Import plugin modules
-from plugins import text_summarizer
-
-# List of all plugin modules for easy importing
-__all__ = ["text_summarizer"] 
